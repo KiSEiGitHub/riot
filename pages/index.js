3051d7f0f748faea48fb2da925575e89a101dce2
KiSEiGitHub <tommusique974@gmail.com>
2022-12-05 23:02:08 +0100

    feat: shortDescReveal

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,10 @@
-import { Button, Image } from "@chakra-ui/react";
+import { Box, Button, Image } from "@chakra-ui/react";
 import { useEffect, useState } from "react";
 
 export default function Home() {
   const [champion, setChampion] = useState({});
   const [isLoading, setIsLoading] = useState(true);
+  const [shortDescReveal, setShortDescReveal] = useState(false);
 
   const getchampion = async () => {
     const res = await fetch(
@@ -28,25 +29,32 @@ export default function Home() {
         {!isLoading &&
           champion.map((item, key) => (
             <div className="champions-cards" key={key}>
+              <Box pos="absolute" display={shortDescReveal ? "block" : "none"}>
+                <h1>Hello</h1>
+              </Box>
               <Image
                 src={`http://ddragon.leagueoflegends.com/cdn/img/champion/splash/${item.name}_0.jpg`}
                 alt="ok"
-                width='100%'
-                height='280px'
-                objectFit='cover'
-                borderTopRadius='10px'
+                width="100%"
+                height="280px"
+                objectFit="cover"
+                borderTopRadius="10px"
+                onMouseEnter={() => setShortDescReveal(true)}
+                onMouseLeave={() => setShortDescReveal(false)}
               />
               <div className="champion-description">
                 <h2 className="champion-name">{item.name}</h2>
                 <h3 className="champion-title">{item.title}</h3>
                 {item.tags.map((el, key) => (
-                  <span className="chamption-tags" key={key}>{el}</span>
+                  <span className="chamption-tags" key={key}>
+                    {el}
+                  </span>
                 ))}
                 <Button
                   mt={4}
-                  display='block'
-                  colorScheme='orange'
-                  variant='outline'
+                  display="block"
+                  colorScheme="orange"
+                  variant="outline"
                 >
                   Lore
                 </Button>
