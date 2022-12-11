3340fbfb99495aa8d79736e2474465b35a7f6967
KiSEiGitHub <tommusique974@gmail.com>
2022-12-11 01:19:31 +0100

    :feat: favicon & title

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,14 @@
 import { ChakraProvider } from "@chakra-ui/react";
+import Head from "next/head";
 import "../styles/global.css";
 import { theme } from "../themes/theme";
 
 function MyApp({ Component, pageProps }) {
   return (
     <ChakraProvider theme={theme}>
+      <Head>
+        <title>Select your champion</title>
+      </Head>
       <Component {...pageProps} />
     </ChakraProvider>
   );
---
+++ pages/_document.js
@@ -0,0 +1,19 @@
+import Document, { Head, Html, Main, NextScript } from 'next/document';
+
+class MyDocument extends Document {
+    render() {
+        return (
+            <Html lang="fr">
+                <Head>
+                    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+                </Head>
+                <body>
+                    <Main />
+                    <NextScript />
+                </body>
+            </Html>
+        );
+    }
+}
+
+export default MyDocument;
